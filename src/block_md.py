c3e42d9ee1e68a92f4e0dba3d95ab4c419a6618a
Harold <harold.vilander@gmail.com>
2025-01-12 07:38:40 -0600

    much progress

--- src/block_md.py
+++ src/block_md.py
@@ -1,17 +1,110 @@
 # block md functions
 
+from enum import Enum
+from htmlnode import HTMLNode
+import re
+
+
+class BlockType(Enum):
+    HEADING = "heading block"
+    CODE = "code block"
+    QUOTE = "quote block"
+    UL = "unordered list"
+    OL = "ordered list"
+    PARAGRAPH = "paragraph"
 
 # document: a string representing a full md doc
 # return a list of strings that is the blocks
 def markdown_to_blocks(document):
+    raw =  document.split('\n\n')
     blocks = []
-    raw =  document.split('\n')
-    for b in document.split('\n'):
-        if b != '':
+    for b in raw:
+        if b != "":
             blocks.append(b.strip())
-
-
-
-
-
     return blocks 
+
+
+# some regex stolen from https://gist.github.com/elfefe/ef08e583e276e7617cd316ba2382fc40
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE 
+
+
+    type = BlockType.PARAGRAPH
+    heading_regex = r"^#{1,6} (.+)"
+    code_regex = r"^`{3}([\S]+)?\n([\s\S]+)\n`{3}$"
+
+    quote_regex = r"^>"
+    unordered_regex = r"^\s*[-+*]\s+(.+)$"
+    ordered_regex = r"^\s*\d+\.\s+(.+)$"
+
+    is_heading = re.match(heading_regex, block)
+    is_code = re.match(code_regex, block)
+    is_quote = re.match(quote_regex, block, re.MULTILINE)
+    is_unordered = re.match(unordered_regex, block, re.MULTILINE)
+    is_ordered = re.match(ordered_regex, block, re.MULTILINE)
+
+    if is_heading: return BlockType.HEADING
+    if is_code: return BlockType.CODE
+    if is_quote: return BlockType.QUOTE
+    if is_ordered: return BlockType.OL
+    if is_unordered: return BlockType.UL
+
+    return type
+    
+def get_heading_lvl(line):
+    count = 0
+    for char in line:
+        if char == '#':
+            count +=1
+        else:
+            break
+
+    return f"h{count}" 
+
+def get_li_for_ul(block):
+    lines = block.split("\n")
+    nodes = []
+    for l in lines:
+        s = 'empty'
+        if l.startswith("- "): 
+            s = l.split("- ")
+        if l.startswith("* "):
+            s = l.split("* ")
+        nodes.append(HTMLNode('li', s[1].strip("\n")))
+
+    return nodes
+
+def block_to_html_node(block):
+    type = block_to_block_type(block)
+    match type:
+        case BlockType.HEADING:
+            tag = get_heading_lvl(block)
+            s = block.split("# ") # TODO handle more than h1
+            return HTMLNode(tag, s[1])
+        case BlockType.CODE:
+            s = block.split("```")
+            child = HTMLNode("code", s[1])
+            return HTMLNode("pre", None, [child])
+        case BlockType.UL:
+            children = get_li_for_ul(block)
+            return HTMLNode("ul", None, children)
+        case _:
+            return HTMLNode("p", block)
+
+
+def markdown_to_html_node(document):
+    blocks = markdown_to_blocks(document)
+    children = []
+    for b in blocks:
+        children.append(block_to_html_node(b))
+
+
+    # HTMLNode(tag, value, children, props):
+    root_node = HTMLNode("div", None, children)
+
+    return root_node
+
--- src/test_block_md.py
+++ src/test_block_md.py
@@ -5,11 +5,122 @@ from block_md import *
 
 class TestBlockMD(unittest.TestCase):
     def test_markdown_to_blocks(self):
-        document = "# Heading \n This is a paragraph \n * bullet 1 \n * bullet 2 \n"
-        expected = ["# Heading", "This is a paragraph", "* bullet 1", "* bullet 2"]
+        document = "# Heading \n\n This is a paragraph \n\n * bullet 1 \n * bullet 2 \n"
+        expected = ["# Heading", "This is a paragraph", "* bullet 1 \n * bullet 2"]
 
         actual = markdown_to_blocks(document) 
 
         self.assertEqual(expected, actual)
 
 
+    def test_block_to_block_type_heading(self):
+        block = "# heading"
+        expected = BlockType.HEADING
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_block_to_block_type_code(self):
+        block = "```\ncode\n```\n"
+        expected = BlockType.CODE
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+
+    def test_block_to_block_type_quote(self):
+        block = "> line one \n >line two"
+        expected = BlockType.QUOTE
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+    def test_block_to_block_type_unordered(self):
+        block = "- line one \n * line two"
+        expected = BlockType.UL
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+
+    def test_block_to_block_type_ordered(self):
+        block = "1. line one \n 2. line two"
+        expected = BlockType.OL
+        actual = block_to_block_type(block)
+        self.assertEqual(expected, actual)
+
+
+    def test_md_to_html_simple(self):
+        document = "# heading\n\n a paragraph"
+        expected = HTMLNode("div", None, [HTMLNode("h1", "heading"), HTMLNode("p", "a paragraph")])
+        actual = markdown_to_html_node(document)
+
+        self.assertEqual(expected, actual)
+
+    def test_md_to_html_code(self):
+        document = "```\nsome \n code\n```\n\n and a paragraph\n"
+        expected_children = [
+            HTMLNode("pre", None, [
+                HTMLNode("code", "\nsome \n code\n")
+            ]),
+            HTMLNode("p", "and a paragraph")
+        ]
+
+        expected = HTMLNode("div", None, expected_children)
+        actual = markdown_to_html_node(document)
+        string_expected = f"{expected}"
+        string_actual = f"{actual}"
+        self.assertEqual(string_expected, string_actual)
+
+
+    def test_md_to_html_ul(self):
+        document = "- one\n- two\n\np after ul"
+        expected_children = [
+            HTMLNode("ul", None, [
+                HTMLNode("li", "one"),
+                HTMLNode("li", "two"), 
+            ]),
+            HTMLNode("p", "p after ul")
+        ]
+
+        expected = HTMLNode("div", None, expected_children)
+        actual = markdown_to_html_node(document)
+        string_expected = f"{expected}"
+        string_actual = f"{actual}"
+        self.assertEqual(string_expected, string_actual)
+
+
+
+    def test_md_to_html_ul_alt(self):
+        document = "* one\n* two\n\np after ul"
+        expected_children = [
+            HTMLNode("ul", None, [
+                HTMLNode("li", "one"),
+                HTMLNode("li", "two"), 
+            ]),
+            HTMLNode("p", "p after ul")
+        ]
+
+        expected = HTMLNode("div", None, expected_children)
+        actual = markdown_to_html_node(document)
+        string_expected = f"{expected}"
+        string_actual = f"{actual}"
+        self.assertEqual(string_expected, string_actual)
+
+
+
+
+
+    def est_md_to_html_node(self):
+        document = "# heading 1\n- ul one\n* ul two\n> start\n> mid\n> end of quote\n```code\nblock\n```\n\n\n1. ol one\n2. ol2\na paragraph\n\n## heading 2\n\n"
+
+        expected_children = [
+            HTMLNode("h1", "heading 1"),
+            HTMLNode("ul", None, [HTMLNode("li", "ul one"), HTMLNode("li", "ul two")]),
+            HTMLNode("blockquote", "start\nmid\nend\n"),
+            HTMLNode("pre", None, HTMLNode("code", "code\nblock")),
+            HTMLNode("ol", None, [HTMLNode("li", "1. ol one"), HTMLNode("li", "n2. ol2")]),
+            HTMLNode("p", "a paragraph"),
+            HTMLNode("h2", "heading 2"),
+        ]
+
+        expected = HTMLNode("div", None, expected_children)
+        actual = markdown_to_html_node(document)
+        self.assertEqual(len(expected.children), len(actual.children))
+        self.assertEqual(expected, actual)
